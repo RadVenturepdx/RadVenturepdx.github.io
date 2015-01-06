5b460f2a2ae46e6f260649fcd36cf9623d5eb1b3
ankur-gupta91 <ankur.gupta91@live.com>
2015-01-05 22:14:31 -0800

    Contact form

--- bin/contact_me.php
+++ bin/contact_me.php
@@ -1,24 +1,26 @@
-<?php 
-// check if fields passed are empty 
- 
- if(empty($_POST['name'])   ||    
-    empty($_POST['email'])  ||
-    empty($_POST['message'])||   
-    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))    
-  {     
-       echo "No arguments Provided!";   return false;    
-  } 
-     
-  $name = $_POST['name']; 
-  $email_address = $_POST['email']; 
-  $message = $_POST['message'];      
- 
- // create email body and send it    
- $to = 'ankgupta@pdx.edu';
- $email_subject = "Contact form submitted by:  $name";
- $email_body = "You have received a new message. \n\n".
-                   " Here are the details:\n \nName: $name \n ".
-                   "Email: $email_address\n Message \n $message";
-
- mail($to,$email_subject,$email_body); return true;
+<?php
+// check if fields passed are empty
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+	
+// create email body and send it	
+$to = 'ankgupta@pdx.edu'; // put your email
+$email_subject = "Contact form submitted by:  $name";
+$email_body = "You have received a new message. \n\n".
+				  " Here are the details:\n \nName: $name \n ".
+				  "Email: $email_address\n Message \n $message";
+$headers = "From: contacts@radventure.com\n";
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
 ?>
